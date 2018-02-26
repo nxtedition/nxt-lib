7510a5eb7cb85e9c17f2ec4b6582cab8bb4cd72d
Robert Nagy <ronagy@icloud.com>
2018-02-26 14:05:23 +0100

    feat: isSameOrNewer

---
+++ deepstream/isSameOrNewer.js
@@ -0,0 +1,25 @@
+module.exports = function isSameOrNewer (a, b) {
+  if (!b) {
+    return true
+  }
+  if (b.startsWith('INF')) {
+    return false
+  }
+  if (!a) {
+    return false
+  }
+  if (a.startsWith('INF')) {
+    return true
+  }
+
+  const [ av, ar ] = splitRev(a)
+  const [ bv, br ] = splitRev(b)
+
+  return av > bv || (av === bv && ar >= br)
+}
+
+function splitRev (s) {
+  const i = s.length - 15
+  const ver = s.slice(0, i)
+  return [ parseInt(ver, 10), s.slice(i + 1) ]
+}
