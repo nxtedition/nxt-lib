ec9556eb3eb212281bfdb009796c4fcb7507bc63
Robert Nagy <ronagy@icloud.com>
2018-04-13 17:11:13 +0200

    refactor(subtract-ranges): further simplify

--- src/subtract-ranges.js
+++ src/subtract-ranges.js
@@ -8,14 +8,12 @@ module.exports = function subtractRanges (a, b) {
 
   while (a.length > 0) {
     const ar = a.shift()
-    const br = b.find(br => ar[0] < br[1] && br[0] < ar[1])
-    if (!br) {
-      c.push(ar)
-    } else {
-      if (br[1] < ar[1]) {
+    if (ar[0] < ar[1]) {
+      const br = b.find(br => ar[0] < br[1] && br[0] < ar[1])
+      if (!br) {
+        c.push(ar)
+      } else {
         a.unshift([ br[1], ar[1] ])
-      }
-      if (ar[0] < br[0]) {
         a.unshift([ ar[0], br[0] ])
       }
     }
