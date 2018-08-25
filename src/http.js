a5daf1b8535fb7e5bfcbde5151611ce362d95207
Robert Nagy <ronagy@icloud.com>
2018-08-25 15:50:01 +0200

    perf(http): defaultLogger instance

--- src/http.js
+++ src/http.js
@@ -2,12 +2,14 @@ const xuid = require('xuid')
 const createLogger = require('./logger')
 const createError = require('http-errors')
 
+const defaultLogger = createLogger()
+
 module.exports.notFound = () => {
   throw createError.NotFound()
 }
 
 module.exports.request = async function request (ctx, next) {
-  const { req, res, logger = createLogger() } = ctx
+  const { req, res, logger = defaultLogger } = ctx
   const startTime = Date.now()
 
   try {
