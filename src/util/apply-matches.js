ac9c6bc1c7a8df0a1150f27ea976e5046e0806d8
Robert Nagy <ronagy@icloud.com>
2018-11-23 14:06:03 +0100

    fix: ensure isString

--- src/util/apply-matches.js
+++ src/util/apply-matches.js
@@ -2,7 +2,7 @@ const fp = require('lodash/fp')
 const balanced = require('balanced-match')
 
 module.exports = function applyMatches (str, matches) {
-  return str && fp.isPlainObject(matches) ? applyMatchesImpl(str, matches) : str
+  return fp.isString(str) && fp.isPlainObject(matches) ? applyMatchesImpl(str, matches) : str
 }
 
 function applyMatchesImpl (str, matches) {
