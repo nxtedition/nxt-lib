b913f28ca51eef2f70f0b67d599c7d05f5e1f644
Robert Nagy <ronagy@icloud.com>
2018-11-23 14:50:27 +0100

    feat: applyMatches string and object mode

--- __tests__/apply-matches.test.js
+++ __tests__/apply-matches.test.js
@@ -6,5 +6,12 @@ test('replaces strings', () => {
   expect(applyMatches('123{{test}}456{{test}}{{test}}', { test: 'asd' })).toBe('123asd456asdasd')
   expect(applyMatches('123{{{{tmp}}}}456{{test}}{{test}}', { test: 'asd', tmp: 'test' })).toBe('123asd456asdasd')
   expect(applyMatches('{{test.foo}}', { test: { foo: 'asd' } })).toBe('asd')
-  expect(applyMatches('{{test.foo2}}', { test: { foo: 'asd' } })).toBe('')
+  expect(applyMatches('{{test.foo2}}', { test: { foo: 'asd' } })).toBe(undefined)
+  expect(applyMatches('`{{test.foo2}}`', { test: { foo: 'asd' } })).toBe('')
+
+  const obj = { asd: 'foo' }
+  expect(applyMatches('{{test.foo}}', { test: { foo: obj } })).toBe(obj)
+  expect(applyMatches('{{test.foo}}{{test.foo}}', { test: { foo: obj } })).toBe(undefined)
+  expect(applyMatches('{{{{test.foo}}}}', { test: { foo: obj } })).toBe(undefined)
+  expect(applyMatches('{{{{test.foo}}}}{{test.foo}}', { test: { foo: obj } })).toBe(undefined)
 })
--- src/util/apply-matches.js
+++ src/util/apply-matches.js
@@ -2,22 +2,79 @@ const fp = require('lodash/fp')
 const balanced = require('balanced-match')
 
 module.exports = function applyMatches (str, matches) {
-  return fp.isString(str) && fp.isPlainObject(matches) ? applyMatchesImpl(str, matches) : str
+  try {
+    return fp.isString(str) && fp.isPlainObject(matches) ? applyMatchesImpl(str, matches) : str
+  } catch (err) {
+
+  }
 }
 
 function applyMatchesImpl (str, matches) {
-  let res = ''
+  let res
+
+  if (/^`.+`$/.test(str)) {
+    // String mode
+    res = ''
+    str = str.slice(1, -1)
+  }
+
   while (true) {
     const match = balanced('{{', '}}', str)
 
     if (!match) {
-      return res + str
+      if (!str) {
+        return res
+      } else if (fp.isString(res)) {
+        return String(res) + String(str)
+      } else if (res === undefined) {
+        return str
+      } else {
+        throw new Error()
+      }
     }
 
     const { pre, body, post } = match
-
-    res += pre
-    res += fp.getOr('', applyMatchesImpl(body, matches), matches)
     str = post
+
+    if (!res && str) {
+      res = ''
+    }
+
+    if (pre) {
+      if (fp.isString(res)) {
+        res = String(res) + pre
+      } else if (res === undefined) {
+        res = pre
+      } else {
+        throw new Error()
+      }
+    }
+
+    let key = applyMatchesImpl(body, matches)
+
+    if (fp.isInteger(key)) {
+      key = String(key)
+    }
+
+    if (!fp.isString(key)) {
+      throw new Error()
+    }
+
+    const val = fp.get(key, matches)
+
+    if (val === undefined) {
+      continue
+    }
+
+    if (fp.isString(res)) {
+      if (!fp.isString(val) && !fp.isInteger(val)) {
+        throw new Error()
+      }
+      res = String(res) + String(val)
+    } else if (res === undefined) {
+      res = val
+    } else {
+      throw new Error()
+    }
   }
 }
