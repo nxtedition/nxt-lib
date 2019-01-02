40a26c90ce61ce0fd8b781f9d3927e5e50752e59
glepur <goran.lepur1@gmail.com>
2019-01-02 13:46:05 +0100

    feat: resolve template, parse simple expressions

--- src/util/resolve-template.js
+++ src/util/resolve-template.js
@@ -1,41 +1,22 @@
-const fp = require('lodash/fp')
 const balanced = require('balanced-match')
 
-module.exports = async function resolveTemplate (str, resolver) {
-  if (fp.isPlainObject(resolver)) {
-    const { map, ds } = resolver
-
-    resolver = async key => {
-      if (!fp.isString(key)) {
-        return ''
-      }
-
-      // {id}:{domain},{path}
-      const [ , id, path ] = key.match(/^\s*([^:\s]+:[^,\s]+)(?:\s*,\s*([^\s]+))?\s*$/) || []
-
-      let val
-      if (id) {
-        val = ds ? await ds.record.get(id, path) : null
-      } else {
-        val = map ? fp.get(key, map) : null
-      }
-
-      return val != null ? String(val) : ''
-    }
+module.exports = async function resolveTemplate (template, context) {
+  let response = ''
+  const match = balanced('{{', '}}', template)
+  if (!match) {
+    return template
   }
-
-  let res = ''
-  while (true) {
-    const match = balanced('${', '}', str)
-
-    if (!match) {
-      return res + str
-    }
-
-    const { pre, body, post } = match
-
-    res += pre
-    res += await resolver(await resolveTemplate(body, resolver))
-    str = post
+  var { pre, body, post } = match
+  response += pre
+  response += getProperty(context, body)
+  if (post) {
+    response += await resolveTemplate(post, context)
   }
+  return response
+}
+
+function getProperty (obj, desc) {
+  var arr = desc.split('.')
+  while (arr.length && (obj = obj[arr.shift()]));
+  return obj
 }
