8781b8f4d1e49969615dc770266f93bc512679cb
glepur <goran.lepur1@gmail.com>
2019-01-02 18:43:04 +0100

    feat: resolve template, parse filter expressions

--- src/util/resolve-template.js
+++ src/util/resolve-template.js
@@ -1,6 +1,8 @@
 const balanced = require('balanced-match')
+const moment = require('moment')
 
-module.exports = async function resolveTemplate (template, context) {
+module.exports = async function resolveTemplate (template, resolver) {
+  const { map, ds } = resolver
   let response = ''
   const match = balanced('{{', '}}', template)
   if (!match) {
@@ -8,15 +10,45 @@ module.exports = async function resolveTemplate (template, context) {
   }
   var { pre, body, post } = match
   response += pre
-  response += getProperty(context, body)
+  response += await parseExpression(body, map, ds)
   if (post) {
-    response += await resolveTemplate(post, context)
+    response += await resolveTemplate(post, { map, ds })
   }
   return response
+}
+
+async function parseExpression (expression, context, ds) {
+  const parts = expression.split('|')
+  const baseValuePath = parts.shift().trim()
+  const baseValue = getProperty(context, baseValuePath)
+
+  return parts.reduce(await applyFilter(ds), Promise.resolve(baseValue))
 }
 
 function getProperty (obj, desc) {
   var arr = desc.split('.')
   while (arr.length && (obj = obj[arr.shift()]));
   return obj
+}
+
+function applyFilter (ds) {
+  return async (valuePromise, rawFilter) => {
+    const value = await valuePromise
+    const filter = rawFilter.trim()
+    const regExp = /\('([^)]+)'\)/
+    const filterValueArr = regExp.exec(filter)
+    const filterValue = filterValueArr && filterValueArr[1]
+
+    if (/^moment\(/.test(filter)) {
+      return moment(value).format(filterValue)
+    } else if (/^append\(/.test(filter)) {
+      return value + filterValue
+    } else if (/^pluck\(/.test(filter)) {
+      return value[filterValue]
+    } else if (/^join\(/.test(filter)) {
+      return value.join(filterValue)
+    } else if (/^ds/.test(filter)) {
+      return ds.record.get(value)
+    }
+  }
 }
