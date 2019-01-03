d8eafcf2e59c5d5eeac94e7c4f2560f2b9637020
Robert Nagy <ronagy@icloud.com>
2019-01-03 12:00:01 +0100

    fix: broken test

--- __tests__/resolve-template.test.js
+++ __tests__/resolve-template.test.js
@@ -1,8 +1,9 @@
 const { resolveTemplate } = require('../src/util')
 
-test('replaces strings', async () => {
+test('replaces strings', async (done) => {
   expect(await resolveTemplate('{{test}}', { test: '111' })).toBe('111')
   expect(await resolveTemplate('pre{{test}}post', { test: '111' })).toBe('pre111post')
   expect(await resolveTemplate('123{{test}}456{{test}}{{test}}', { test: 'body' })).toBe('123body456bodybody')
   expect(await resolveTemplate('test{{test.foo}}test{{test.bar.baz}}test', { test: { foo: '111', bar: { baz: '222' } } })).toBe('test111test222test')
+  done()
 })
