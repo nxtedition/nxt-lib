8c6f13704b6c04bbea0c7547a62ee034478f1e27
Martin Grönlund <martingronlund@live.se>
2019-01-04 09:57:18 +0100

    feat(resolve-template): more filters + better parsing

--- __tests__/resolve-template.test.js
+++ __tests__/resolve-template.test.js
@@ -1,5 +1,10 @@
 const { resolveTemplate } = require('../src/util')
 const Observable = require('rxjs')
+const mapValues = require('lodash/mapValues')
+const pickBy = require('lodash/pickBy')
+require('jasmine-check').install(global)
+
+const { check, gen } = global
 
 test('replaces strings', async (done) => {
   expect(await resolveTemplate('{{test}}', { test: '111' })).toBe('111')
@@ -27,4 +32,86 @@ test('ds', async (done) => {
   }
   expect(await resolveTemplate(`{{test | ds() | pluck('foo')}}`, { test: 'foo' }, { ds })).toBe('bar')
   done()
+})
+
+describe('map', async () => {
+  await Promise.all([
+    check.it(
+      'treats empty call as noop',
+      gen.object({ test: gen.array(gen.int) }),
+      gen.int,
+      (x, y) => {
+        const { test } = x
+        const t = `{{ test | map() }}`
+        const e = JSON.stringify(test)
+        const r = resolveTemplate(t, x)
+        return expect(r).resolves.toBe(e)
+      }
+    ),
+    check.it(
+      'maps arrays',
+      gen.object({ test: gen.array(gen.int) }),
+      gen.int,
+      (x, y) => {
+        const { test } = x
+        const t = `{{ test | map('mul', ${y}) }}`
+        const e = JSON.stringify(test.map(x => x * y))
+        const r = resolveTemplate(t, x)
+        return expect(r).resolves.toBe(e)
+      }
+    ),
+    check.it(
+      'maps object values',
+      gen.object({ test: gen.object(gen.string, gen.int) }),
+      gen.int,
+      (x, y) => {
+        const { test } = x
+        const t = `{{ test | map('mul', ${y}) }}`
+        const e = JSON.stringify(mapValues(test, x => x * y))
+        const r = resolveTemplate(t, x)
+        return expect(r).resolves.toBe(e)
+      }
+    )
+  ])
+})
+
+describe('select', async () => {
+  await Promise.all([
+    check.it(
+      'treats empty call as Boolean filter',
+      gen.object({ test: gen.array(gen.int) }),
+      gen.int,
+      (x, y) => {
+        const { test } = x
+        const t = `{{ test | select() }}`
+        const e = JSON.stringify(test.filter(Boolean))
+        const r = resolveTemplate(t, x)
+        return expect(r).resolves.toBe(e)
+      }
+    ),
+    check.it(
+      'filters arrays',
+      gen.object({ test: gen.array(gen.int) }),
+      gen.int,
+      (x, y) => {
+        const { test } = x
+        const t = `{{ test | select('gte', ${y}) }}`
+        const e = JSON.stringify(test.filter(x => x >= y))
+        const r = resolveTemplate(t, x)
+        return expect(r).resolves.toBe(e)
+      }
+    ),
+    check.it(
+      'filters object entries',
+      gen.object({ test: gen.object(gen.string, gen.int) }),
+      gen.int,
+      (x, y) => {
+        const { test } = x
+        const t = `{{ test | select('gte', ${y}) }}`
+        const e = JSON.stringify(pickBy(test, x => x >= y))
+        const r = resolveTemplate(t, x)
+        return expect(r).resolves.toBe(e)
+      }
+    )
+  ])
 })
