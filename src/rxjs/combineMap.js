ab65e453b98c2f7a2598fae9bc5ecafcbfceb04a
Robert Nagy <ronagy@icloud.com>
2019-01-25 12:23:56 +0100

    fix(rxjs): combineMap

--- src/rxjs/combineMap.js
+++ src/rxjs/combineMap.js
@@ -1,7 +1,7 @@
 const { Observable } = require('rxjs')
 
-module.exports = Observable.prototype.combineMap = function (xs$, resolver) {
-  return xs$.switchMap(xs => Array.isArray(xs) && xs.length > 0
+module.exports = Observable.prototype.combineMap = function (resolver) {
+  return this.switchMap(xs => Array.isArray(xs) && xs.length > 0
     ? Observable.combineLatest(xs.map(resolver))
     : Observable.of([])
   )
