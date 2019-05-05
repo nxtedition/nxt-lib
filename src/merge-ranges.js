d94e61bccb49212a38503cabeb36982dc0c44f25
Robert Nagy <ronagy@icloud.com>
2019-05-05 15:17:48 +0200

    fix(merge-ranges): remove empty ranges

--- src/merge-ranges.js
+++ src/merge-ranges.js
@@ -2,4 +2,5 @@ const mergeRangesImpl = require('merge-ranges')
 
 module.exports = function mergeRanges (a) {
   return mergeRangesImpl(JSON.parse(JSON.stringify(a)))
+    .filter(([ start, end ]) => end > start)
 }
