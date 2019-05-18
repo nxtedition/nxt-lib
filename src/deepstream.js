5f19e7a991c4d5ecb263304eac512b6843e1b1a2
Robert Nagy <ronagy@icloud.com>
2019-05-18 16:54:53 +0200

    fix(deepstream): expr

--- src/deepstream.js
+++ src/deepstream.js
@@ -12,14 +12,14 @@ function provide (ds, domain, callback, options) {
     callback = cached(callback, options, (id, options, key) => key)
   }
 
-  return ds.record.provide(`^(.+:)?${domain.replace('.', '\\.')}(\\?.*)$`, key => {
+  return ds.record.provide(`^(.*:)?${domain.replace('.', '\\.')}(\\?.*)?$`, key => {
     const [ id, options ] = parseKey(key)
     return callback(id, options, key)
   }, options.recursive)
 }
 
 function parseKey (key) {
-  const { json, id, query } = key.match(/^(?:(?<json>\{.+\})|(?<id>.+):)?[^?]*(?:\?(?<query>.+))?$/).groups
+  const { json, id, query } = key.match(/^(?:(?<json>\{.+\})|(?<id>.*):)?[^?]*(?:\?(?<query>.*))?$/).groups
   if (query) {
     return [ id || '', querystring.parse(query) ]
   } else if (json) {
