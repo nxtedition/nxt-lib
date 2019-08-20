ce8fa16ae670de318eed2a57be77d374efd96306
Robert Nagy <ronagy@icloud.com>
2019-08-20 14:00:30 +0200

    fix(destroy): there is not always callback

--- src/destroy.js
+++ src/destroy.js
@@ -5,6 +5,13 @@ module.exports = function destroy (self, err, callback) {
   callback = callback ? once(callback) : null
   // Ensure no uncaught exception if we don't care about errors.
   self.on('error', callback || noop)
+  if (callback) {
+    self
+      // node doesn't always implement destroy with callback
+      .on('close', callback)
+      // node doesn't always emit 'close'
+      .on('end', callback)
+  }
   if (typeof self.abort === 'function') {
     self.abort() // Fix for ClientRequest.
   } else {
