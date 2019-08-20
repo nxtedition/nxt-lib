5a8b83b4449ccd7eb9d8b0f869885879e8d4dd1c
Robert Nagy <ronagy@icloud.com>
2019-08-20 12:42:08 +0200

    docs(destroy): comment

--- src/util/destroy.js
+++ src/util/destroy.js
@@ -3,6 +3,7 @@ const noop = () => {}
 // If you want to know about errors, pass callback.
 module.exports = function destroy (self, err, callback) {
   callback = callback ? once(callback) : null
+  // Ensure no uncaught exception if we don't care about errors.
   self.on('error', callback || noop)
   if (typeof self.abort === 'function') {
     self.abort() // Fix for ClientRequest.
