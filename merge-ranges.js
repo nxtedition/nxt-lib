dfde0cc6c36460137a6fa3af2c3d7e7ea3b4f8da
Robert Nagy <ronagy@icloud.com>
2020-01-31 14:11:38 +0100

    fix: removed build files

--- merge-ranges.js
+++
@@ -1,6 +0,0 @@
-const mergeRangesImpl = require('merge-ranges')
-
-module.exports = function mergeRanges (a) {
-  return mergeRangesImpl(JSON.parse(JSON.stringify(a)))
-    .filter(([start, end]) => end > start)
-}
