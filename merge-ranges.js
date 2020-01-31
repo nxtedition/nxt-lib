169d65d78b1dfd50aa85cfd2bb544c837c18b165
Robert Nagy <ronagy@icloud.com>
2020-01-31 14:14:33 +0100

    fix(app): cache loging

---
+++ merge-ranges.js
@@ -0,0 +1,6 @@
+const mergeRangesImpl = require('merge-ranges')
+
+module.exports = function mergeRanges (a) {
+  return mergeRangesImpl(JSON.parse(JSON.stringify(a)))
+    .filter(([start, end]) => end > start)
+}
