3b6fe494285102b7e75af11ec1ca8fd6001cebd5
Robert Nagy <ronagy@icloud.com>
2020-03-09 10:59:40 +0100

    fix(app): dump + stats

--- src/app.js
+++ src/app.js
@@ -16,6 +16,13 @@ module.exports = function (config, onTerminate) {
   if (config.deepstream) {
     const deepstream = require('@nxtedition/deepstream.io-client-js')
     const cacheDb = config.deepstream.cache ? require('leveldown')(config.deepstream.cache) : null
+    const xuid = require('xuid')
+    const fsp = require('fs').promises
+    const fs = require('fs')
+    const stream = require('stream')
+    const pipeline = require('util').promisify(stream.pipeline)
+    const os = require('os')
+
     if (cacheDb) {
       logger.debug({ cache: config.deepstream.cache }, 'Deepstream Caching')
     }
@@ -45,6 +52,46 @@ module.exports = function (config, onTerminate) {
       .on('error', err => {
         logger.error({ err }, 'Deepstream Error.')
       })
+
+    ds.rpc.provide(`${config.isProduction ? os.hostname() : module}.dump`, async () => {
+      const path = `./${xuid()}.subscriptions`
+      await pipeline(
+        stream.Readable.from(ds.record._records.entries()),
+        stream.Transform({
+          objectMode: true,
+          transform ([key, val], encoding, callback) {
+            callback(null, `${key} ${val.version} ${val.state}`)
+          }
+        }),
+        fs.createWriteStream(path + '.tmp')
+      )
+      await fsp.rename(path + '.tmp', path)
+    })
+  }
+
+  if (config.stats) {
+    const v8 = require('v8')
+
+    let stats = {}
+    if (config.stats.subscribe) {
+      config.stats.subscribe(x => {
+        stats = x
+      })
+    } else if (config.stats === 'object') {
+      stats = config.stats
+    }
+
+    setInterval(() => {
+      logger.debug({
+        ds: ds.stats,
+        memory: process.memoryUsage(),
+        v8: {
+          heapSpace: v8.getHeapSpaceStatistics(),
+          heap: v8.getHeapStatistics()
+        },
+        ...stats
+      }, 'STATS')
+    }, 10e3)
   }
 
   return { ds, logger, toobusy }
