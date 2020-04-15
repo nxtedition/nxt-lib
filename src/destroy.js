4c1e34bf4554fb92976d5bc42abde6a1cbd2ae2c
Robert Nagy <ronagy@icloud.com>
2020-04-15 15:38:14 +0200

    fix: destroy

--- src/destroy.js
+++ src/destroy.js
@@ -1,4 +1,3 @@
-const once = require('once')
 const { finished } = require('stream')
 const noop = () => {}
 // If you want to know about errors, pass callback.
@@ -7,25 +6,7 @@ module.exports = function destroy (self, err, callback) {
     callback = err
     err = null
   }
-  callback = callback ? once(callback) : null
-  // Ensure no uncaught exception if we don't care about errors.
-  self.on('error', callback || noop)
-  if (callback) {
-    self
-      // node doesn't always implement destroy with callback
-      .on('close', callback)
-      // node doesn't always emit 'close' after 'aborted'
-      // aborted should be ECONNRESET.
-      .on('aborted', () => {
-        const err = new Error('aborted')
-        err.code = 'ECONNRESET'
-        callback(err)
-      })
-      // node doesn't always emit 'close'
-      .on('end', callback)
-      .on('finish', callback)
-    finished(self, callback)
-  }
+  finished(self, callback || noop)
   if (typeof self.abort === 'function') {
     self.abort() // Fix for ClientRequest.
   } else {
