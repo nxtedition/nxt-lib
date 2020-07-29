842fc35a882227b965d82c2a3a13772ebaf1adfb
Robert Nagy <ronagy@icloud.com>
2020-07-29 10:33:48 +0200

    fix(couch): headersTimeout

--- couch.js
+++ couch.js
@@ -85,13 +85,15 @@ module.exports = function ({ config }) {
     params.heartbeat = Number.isFinite(params.heartbeat) ? params.heartbeat : 30e3
 
     return Observable.create(o => {
+      const timeout = 2 * (Number.isFinite(params.heartbeat) ? params.heartbeat : 30e3)
       const userClient = options.client
       const client = userClient || createClient({
         protocol,
         hostname,
         port
       }, {
-        socketTimeout: 2 * (Number.isFinite(params.heartbeat) ? params.heartbeat : 30e3)
+        socketTimeout: timeout,
+        headersTimeout: timeout
       })
       let buf = ''
       const subscription = onRequest('/_changes', {
