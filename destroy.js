e6187829b9fef0455edbe469ad914011bfe83e23
Robert Nagy <ronagy@icloud.com>
2021-01-01 23:20:50 +0100

    fix(destroy): generic

--- destroy.js
+++ destroy.js
@@ -1,12 +1,43 @@
 const { finished } = require('stream')
-const noop = () => {}
+const { kDestroyed } = require('./symbols')
+const { IncomingMessage } = require('http')
 
-// If you want to know about errors, pass callback.
-module.exports = function destroy (self, err, callback) {
+function isStream (body) {
+  return !!(body && typeof body.on === 'function')
+}
+
+function isDestroyed (stream) {
+  return !stream || !!(stream.destroyed || stream[kDestroyed])
+}
+
+function destroy (stream, err, callback) {
   if (typeof err === 'function') {
     callback = err
     err = null
   }
-  finished(self, callback || noop)
-  self.destroy(err)
+
+  if (!isStream(stream) || isDestroyed(stream)) {
+    process.nextTick(callback)
+    return
+  }
+
+  if (typeof stream.destroy === 'function') {
+    finished(stream, err => callback(err || null))
+    if (err || Object.getPrototypeOf(stream).constructor !== IncomingMessage) {
+      stream.destroy(err)
+    }
+  } else if (err) {
+    process.nextTick((stream, err) => {
+      stream.emit('error', err)
+      process.nextTick(callback, err)
+    }, stream, err)
+  } else {
+    setImmediate(callback)
+  }
+
+  if (stream.destroyed !== true) {
+    stream[kDestroyed] = true
+  }
 }
+
+module.exports = destroy
