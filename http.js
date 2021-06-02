41dbc4fc548f58ec4a44be7f49443a2e4bfdcd14
Robert Nagy <ronagy@icloud.com>
2021-06-02 13:45:01 +0200

    fix(http): genReqId

--- http.js
+++ http.js
@@ -6,7 +6,8 @@ const assert = require('assert')
 const AbortController = require('abort-controller')
 const { AbortError } = require('./errors')
 
-const ERR_HEADER_EXPR = /^(content-length|content-type|te|host|upgrade|trailers|connection|keep-alive|http2-settings|transfer-encoding|proxy-connection|proxy-authenticate|proxy-authorization)$/i
+const ERR_HEADER_EXPR =
+  /^(content-length|content-type|te|host|upgrade|trailers|connection|keep-alive|http2-settings|transfer-encoding|proxy-connection|proxy-authenticate|proxy-authorization)$/i
 
 // https://github.com/fastify/fastify/blob/main/lib/reqIdGenFactory.js
 // 2,147,483,647 (2^31 − 1) stands for max SMI value (an internal optimization of V8).
@@ -15,10 +16,10 @@ const ERR_HEADER_EXPR = /^(content-length|content-type|te|host|upgrade|trailers|
 // Growing beyond this value will make the id generation slower and cause a deopt.
 // In the worst cases, it will become a float, losing accuracy.
 const maxInt = 2147483647
-let nextReqId = Math.floor(Math.random() * 2147483647)
-function genReqId(req) {
+let nextReqId = Math.floor(Math.random() * maxInt)
+function genReqId() {
   nextReqId = (nextReqId + 1) & maxInt
-  return `req-${nextReqId.toString(36)}`
+  return nextReqId.toString(36)
 }
 
 module.exports.request = async function request(ctx, next) {
