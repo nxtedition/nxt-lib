bbef0a499696db6cb436304e1d4aad83b820a28f
Robert Nagy <ronagy@icloud.com>
2021-09-03 08:31:10 +0200

    style: prettier

--- util/template/expression.js
+++ util/template/expression.js
@@ -11,17 +11,18 @@ const weakCache = require('../../weakCache')
 
 function asFilter(transform, predicate, obj) {
   return fp.mapValues(
-    (factory) => (...args) => {
-      const filter = factory(...args)
-      return (value) => {
-        try {
-          value = transform ? transform(value) : value
-          return !predicate || predicate(value) ? filter(value) : null
-        } catch (err) {
-          return null
+    (factory) =>
+      (...args) => {
+        const filter = factory(...args)
+        return (value) => {
+          try {
+            value = transform ? transform(value) : value
+            return !predicate || predicate(value) ? filter(value) : null
+          } catch (err) {
+            return null
+          }
         }
-      }
-    },
+      },
     obj
   )
 }
@@ -38,21 +39,25 @@ module.exports = ({ ds } = {}) => {
       json5: () => (value) => JSON5.stringify(value),
       string: () => (value) => String(value),
       number: () => (value) => Number(value),
-      date: (tz) => (value) => (tz ? moment.tz(value, tz) : moment(value)),
+      date: (tz) => (value) => tz ? moment.tz(value, tz) : moment(value),
       array: () => (value) => [value],
       value: (value) => () => value,
-      int: (fallback = null, radix) => (value) => {
-        // TODO (fix): Validate arguments...
+      int:
+        (fallback = null, radix) =>
+        (value) => {
+          // TODO (fix): Validate arguments...
 
-        const int = parseInt(value, radix)
-        return Number.isFinite(int) ? int : fallback
-      },
-      float: (fallback = null) => (value) => {
-        // TODO (fix): Validate arguments...
+          const int = parseInt(value, radix)
+          return Number.isFinite(int) ? int : fallback
+        },
+      float:
+        (fallback = null) =>
+        (value) => {
+          // TODO (fix): Validate arguments...
 
-        const float = parseFloat(value)
-        return Number.isFinite(float) ? float : fallback
-      },
+          const float = parseFloat(value)
+          return Number.isFinite(float) ? float : fallback
+        },
       default: (defaultValue, notJustNully) => (value) =>
         notJustNully ? (!value ? defaultValue : value) : value == null ? defaultValue : value,
       eq: (x) => (value) => value === x,
@@ -66,30 +71,34 @@ module.exports = ({ ds } = {}) => {
       isNil: () => (value) => value == null,
       isNumber: () => (value) => Number.isFinite(value),
       isString: () => (value) => fp.isString(value),
-      ternary: (a, b) => (value) => (value ? a : b),
-      cond: (a, b) => (value) => (value ? a : b),
+      ternary: (a, b) => (value) => value ? a : b,
+      cond: (a, b) => (value) => value ? a : b,
       hasha: (options) => (value) => hasha(JSON.stringify(value), options || {}),
       hashaint: (options) => (value) =>
         parseInt(hasha(JSON.stringify(value), options || {}).slice(-13), 16),
       return: () => (value) => value || RETURN,
-      add: (...args) => (value) => {
-        if (args.length === 2) {
-          value = moment(value)
-          return value.isValid() ? value.add(...args) : null
-        } else {
-          value = Number(value)
-          return Number.isFinite(value) ? value + args[0] : null
-        }
-      },
-      sub: (...args) => (value) => {
-        if (args.length === 2) {
-          value = moment(value)
-          return value.isValid() ? value.subtract(...args) : null
-        } else {
-          value = Number(value)
-          return Number.isFinite(value) ? value - args[0] : null
-        }
-      },
+      add:
+        (...args) =>
+        (value) => {
+          if (args.length === 2) {
+            value = moment(value)
+            return value.isValid() ? value.add(...args) : null
+          } else {
+            value = Number(value)
+            return Number.isFinite(value) ? value + args[0] : null
+          }
+        },
+      sub:
+        (...args) =>
+        (value) => {
+          if (args.length === 2) {
+            value = moment(value)
+            return value.isValid() ? value.subtract(...args) : null
+          } else {
+            value = Number(value)
+            return Number.isFinite(value) ? value - args[0] : null
+          }
+        },
     }),
     // number
     ...asFilter(
@@ -117,8 +126,14 @@ module.exports = ({ ds } = {}) => {
           .filter((x) => Number.isFinite(x)),
       (value) => value.every((x) => Number.isFinite(x)),
       {
-        min: (...args) => (value) => Math.min(...value, ...args),
-        max: (...args) => (value) => Math.max(...value, ...args),
+        min:
+          (...args) =>
+          (value) =>
+            Math.min(...value, ...args),
+        max:
+          (...args) =>
+          (value) =>
+            Math.max(...value, ...args),
       }
     ),
     // date
@@ -179,8 +194,8 @@ module.exports = ({ ds } = {}) => {
         value == null || fp.isPlainObject(value) || Array.isArray(value) ? '' : String(value),
       (value) => typeof value === 'string',
       {
-        fromJSON: () => (value) => (value ? JSON.parse(value) : null),
-        fromJSON5: () => (value) => (value ? JSON5.parse(value) : null),
+        fromJSON: () => (value) => value ? JSON.parse(value) : null,
+        fromJSON5: () => (value) => value ? JSON5.parse(value) : null,
         toSlate: () => (value) => ({
           object: 'value',
           document: {
@@ -259,48 +274,71 @@ module.exports = ({ ds } = {}) => {
             throw new Error('invalid argument')
           }
         },
-        padStart: (...args) => (value) => value.padStart(...args),
-        padEnd: (...args) => (value) => value.padEnd(...args),
-        charAt: (...args) => (value) => value.charAt(...args),
-        startsWith: (...args) => (value) => value.startsWith(...args),
-        endsWith: (...args) => (value) => value.endsWith(...args),
-        substring: (...args) => (value) => value.substring(...args),
-        slice: (...args) => (value) => value.slice(...args),
+        padStart:
+          (...args) =>
+          (value) =>
+            value.padStart(...args),
+        padEnd:
+          (...args) =>
+          (value) =>
+            value.padEnd(...args),
+        charAt:
+          (...args) =>
+          (value) =>
+            value.charAt(...args),
+        startsWith:
+          (...args) =>
+          (value) =>
+            value.startsWith(...args),
+        endsWith:
+          (...args) =>
+          (value) =>
+            value.endsWith(...args),
+        substring:
+          (...args) =>
+          (value) =>
+            value.substring(...args),
+        slice:
+          (...args) =>
+          (value) =>
+            value.slice(...args),
         trim: () => (value) => value.trim(),
         trimStart: () => (value) => value.trimStart(),
         trimEnd: () => (value) => value.trimEnd(),
         trimLeft: () => (value) => value.trimLeft(),
         trimRight: () => (value) => value.trimRight(),
-        truncate: (length = 255, killwords = false, end = '...', leeway = 0) => (value) => {
-          // TODO (fix): Validate arguments...
+        truncate:
+          (length = 255, killwords = false, end = '...', leeway = 0) =>
+          (value) => {
+            // TODO (fix): Validate arguments...
 
-          const s = String(value)
+            const s = String(value)
 
-          if (length < end.length) {
-            length = end.length
-          }
+            if (length < end.length) {
+              length = end.length
+            }
 
-          if (leeway < 0) {
-            leeway = 0
-          }
+            if (leeway < 0) {
+              leeway = 0
+            }
 
-          if (s.length <= length + leeway) {
-            return s
-          }
+            if (s.length <= length + leeway) {
+              return s
+            }
 
-          if (killwords) {
-            return s.slice(0, length - end.length) + end
-          }
+            if (killwords) {
+              return s.slice(0, length - end.length) + end
+            }
 
-          return (
-            s
-              .slice(0, length - end.length)
-              .trimRight()
-              .split(' ')
-              .slice(0, -1)
-              .join(' ') + end
-          )
-        },
+            return (
+              s
+                .slice(0, length - end.length)
+                .trimRight()
+                .split(' ')
+                .slice(0, -1)
+                .join(' ') + end
+            )
+          },
         wordcount: () => (value) => fp.words(value).length,
         words: () => (value) => fp.words(value),
       }
@@ -309,7 +347,10 @@ module.exports = ({ ds } = {}) => {
       (value) => (Array.isArray(value) || typeof value === 'string' ? value : []),
       (value) => value.includes,
       {
-        includes: (...args) => (value) => value.includes(...args),
+        includes:
+          (...args) =>
+          (value) =>
+            value.includes(...args),
       }
     ),
     // array
@@ -319,14 +360,14 @@ module.exports = ({ ds } = {}) => {
       {
         map: (path) => (value) =>
           fp.isArray(path) ? fp.map(fp.pick(path), value) : fp.map(fp.get(path), value),
-        every: (predicate) => value => fp.every(predicate, value),
-        some: (predicate) => value => fp.some(predicate, value),
-        filter: (predicate) => value => fp.filter(predicate, value),
-        reject: (predicate) => value => fp.reject(predicate, value),
-        findIndex: (predicate) => value => fp.findIndex(predicate, value),
-        findLastIndex: (predicate) => value => fp.findLastIndex(predicate, value),
-        find: (predicate) => value => fp.find(predicate, value),
-        findLast: (predicate) => value => fp.findLast(predicate, value),
+        every: (predicate) => (value) => fp.every(predicate, value),
+        some: (predicate) => (value) => fp.some(predicate, value),
+        filter: (predicate) => (value) => fp.filter(predicate, value),
+        reject: (predicate) => (value) => fp.reject(predicate, value),
+        findIndex: (predicate) => (value) => fp.findIndex(predicate, value),
+        findLastIndex: (predicate) => (value) => fp.findLastIndex(predicate, value),
+        find: (predicate) => (value) => fp.find(predicate, value),
+        findLast: (predicate) => (value) => fp.findLast(predicate, value),
         slice: (start, end) => (value) => value.slice(start, end),
         reverse: () => (value) => fp.reverse(value),
         join: (delimiter) => (value) => fp.join(delimiter, value),
@@ -337,19 +378,37 @@ module.exports = ({ ds } = {}) => {
         tail: () => (value) => fp.last(value),
         length: () => (value) => fp.size(value),
         sort: () => (value) => [...value].sort(),
-        sortBy: (iteratees) => value => fp.sortBy(iteratees, value),
+        sortBy: (iteratees) => (value) => fp.sortBy(iteratees, value),
         sum: () => (value) => fp.sum(value),
         unique: () => (value) => fp.uniq(value),
         uniq: () => (value) => fp.uniq(value),
-        flatten: (depth = 1) => (value) => fp.flattenDepth(depth, value),
+        flatten:
+          (depth = 1) =>
+          (value) =>
+            fp.flattenDepth(depth, value),
         flattenDeep: () => (value) => fp.flattenDeep(value),
-        union: (...args) => (value) => fp.union(args[0], value),
-        intersection: (...args) => (value) => fp.intersection(args[0], value),
-        concat: (...args) => (value) => fp.concat(value, args[0]),
-        difference: (...args) => (value) => fp.difference(value, args[0]),
+        union:
+          (...args) =>
+          (value) =>
+            fp.union(args[0], value),
+        intersection:
+          (...args) =>
+          (value) =>
+            fp.intersection(args[0], value),
+        concat:
+          (...args) =>
+          (value) =>
+            fp.concat(value, args[0]),
+        difference:
+          (...args) =>
+          (value) =>
+            fp.difference(value, args[0]),
         initial: () => (value) => fp.initial(value),
         compact: () => (value) => fp.compact(value),
-        pull: (...args) => (value) => fp.pull(args[0], value),
+        pull:
+          (...args) =>
+          (value) =>
+            fp.pull(args[0], value),
       }
     ),
     // collection
