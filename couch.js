219e9378382b5b76b6b520ed931539f40c9d4611
Robert Nagy <ronagy@icloud.com>
2021-09-10 14:53:47 +0200

    fix(couch): batch_size

--- couch.js
+++ couch.js
@@ -156,7 +156,10 @@ module.exports = function (opts) {
               ...params,
               ...options.query,
               heartbeat: Number.isFinite(params.heartbeat) ? params.heartbeat : 10e3,
-              limit: Math.min(remaining, options.batchSize ?? (params.include_docs ? 256 : 1024)),
+              limit: Math.min(
+                remaining,
+                options.batch_size ?? options.batchSize ?? (params.include_docs ? 256 : 1024)
+              ),
               feed: live ? 'longpoll' : 'normal',
             })}`,
           idempotent: true,
