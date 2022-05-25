b22f2ad33497688967f1a40c16ff7a301c082c07
Robert Nagy <ronagy@icloud.com>
2022-05-25 07:21:12 +0200

    perf: reimplement faster combineMap (#30)

--- rxjs/combineMap.js
+++ rxjs/combineMap.js
@@ -1,14 +1,137 @@
 const rxjs = require('rxjs')
-const rx = require('rxjs/operators')
 
-function combineMap(resolver) {
-  return this.pipe(
-    rx.switchMap((xs) =>
-      Array.isArray(xs) && xs.length > 0 ? rxjs.combineLatest(xs.map(resolver)) : rxjs.of([])
-    )
-  )
+const EMPTY = {}
+
+function combineMap(project, compare = (a, b) => a === b) {
+  const self = this
+  return new rxjs.Observable((o) => {
+    let curr = []
+    let scheduled = false
+    let updated = false
+    let active = 0
+    let empty = 0
+
+    const _error = (err) => o.error(err)
+
+    function _update() {
+      scheduled = false
+
+      if (empty) {
+        return
+      }
+
+      if (updated) {
+        updated = false
+        o.next(curr.map((context) => context.value))
+      }
+
+      if (!active) {
+        o.complete()
+      }
+    }
+
+    function update() {
+      if (!scheduled && !empty) {
+        scheduled = true
+        process.nextTick(_update)
+      }
+    }
+
+    active += 1
+    const subscription = self.subscribe({
+      next(keys) {
+        // TODO (perf): Avoid array allocation & copy if nothing has updated.
+
+        const len = Array.isArray(keys) ? keys.length : 0
+        const next = new Array(len)
+
+        for (let n = 0; n < len; ++n) {
+          const key = keys[n]
+
+          if (n < curr.length && compare(curr[n].key, key)) {
+            next[n] = curr[n]
+            curr[n] = null
+            continue
+          }
+
+          updated = true
+          update()
+
+          // TODO (perf): Guess start index based on n, e.g. n - 1 and n + 1 to check if
+          // a key has simply been added or removed.
+          const i = curr.findIndex((context) => context && compare(context.key, key))
+
+          if (i !== -1) {
+            next[n] = curr[i]
+            curr[i] = null
+          } else {
+            const context = {
+              key,
+              value: EMPTY,
+              subscription: null,
+            }
+
+            let observable
+            try {
+              observable = rxjs.from(project(keys[n]))
+            } catch (err) {
+              observable = rxjs.throwError(() => err)
+            }
+
+            empty += 1
+            active += 1
+            context.subscription = observable.subscribe({
+              next(value) {
+                if (context.value === EMPTY) {
+                  empty -= 1
+                }
+
+                context.value = value
+
+                updated = true
+                update()
+              },
+              error: _error,
+            })
+            context.subscription.add(() => {
+              if (context.value === EMPTY) {
+                empty -= 1
+              }
+
+              active -= 1
+
+              updated = true
+              update()
+            })
+
+            next[n] = context
+          }
+        }
+
+        for (const context of curr) {
+          context?.subscription.unsubscribe()
+        }
+
+        curr = next
+      },
+      error: _error,
+      complete() {
+        active -= 1
+        if (!active) {
+          update()
+        }
+      },
+    })
+
+    return () => {
+      for (const context of curr) {
+        context?.subscription.unsubscribe()
+      }
+      subscription.unsubscribe()
+    }
+  })
 }
 
 rxjs.Observable.prototype.combineMap = combineMap
 
-module.exports = (resolver) => (o) => combineMap.call(o, resolver)
+module.exports = (project, compare) => (o) => combineMap.call(o, project, compare)
---
+++ test/rxjs/combineMap.js
@@ -0,0 +1,164 @@
+const { test } = require('tap')
+const combineMap = require('../../rxjs/combineMap')
+const rxjs = require('rxjs')
+
+test('combineMap sync', (t) => {
+  t.plan(1)
+  rxjs
+    .of([1, 2, 3])
+    .pipe(combineMap((val) => rxjs.of(val * 2)))
+    .subscribe((val) => {
+      t.same(val, [2, 4, 6])
+    })
+})
+
+test('combineMap async', (t) => {
+  t.plan(1)
+  rxjs
+    .of([1, 2, 3])
+    .pipe(combineMap(async (val) => val * 2))
+    .subscribe((val) => {
+      t.same(val, [2, 4, 6])
+    })
+})
+
+test('combineMap throw in resolver', (t) => {
+  t.plan(1)
+  const _err = new Error('asd')
+  rxjs
+    .of([1, 2, 3])
+    .pipe(
+      combineMap((val) => {
+        throw _err
+      })
+    )
+    .subscribe({
+      error: (err) => {
+        t.same(err, _err)
+      },
+    })
+})
+
+test('combineMap throw in source', (t) => {
+  t.plan(1)
+  const _err = new Error('asd')
+  rxjs
+    .throwError(() => _err)
+    .pipe(combineMap((val) => rxjs.of(val)))
+    .subscribe({
+      error: (err) => {
+        t.same(err, _err)
+      },
+    })
+})
+
+test('combineMap bad resolve', (t) => {
+  t.plan(1)
+  rxjs
+    .of([1])
+    .pipe(combineMap((val) => val))
+    .subscribe({
+      error: () => {
+        t.pass()
+      },
+    })
+})
+
+test('combineMap no change no tick', (t) => {
+  t.plan(1)
+  rxjs
+    .concat(
+      rxjs.timer(10).pipe(rxjs.map(() => [1, 2, 3])),
+      rxjs.timer(10).pipe(rxjs.map(() => [1, 2, 3]))
+    )
+    .pipe(combineMap((val) => rxjs.of(val * 2)))
+    .subscribe(() => {
+      t.pass()
+    })
+})
+
+test('combineMap combine in single tick', (t) => {
+  t.plan(2)
+  rxjs
+    .concat(
+      rxjs.timer(10).pipe(rxjs.map(() => [1, 2, 3])),
+      rxjs.timer(10).pipe(rxjs.map(() => [4, 5, 6]))
+    )
+    .pipe(combineMap((val) => rxjs.from([val * 2, val * 2])))
+    .subscribe(() => {
+      t.pass()
+    })
+})
+
+test('combineLatest completion', (t) => {
+  t.plan(1)
+  rxjs.combineLatest([1, 2, 3].map((x) => rxjs.of(x))).subscribe({
+    complete: () => {
+      t.pass()
+    },
+  })
+})
+
+test('combineMap completion', (t) => {
+  t.plan(1)
+  rxjs
+    .of([1, 2, 3])
+    .pipe(combineMap((x) => rxjs.of(x)))
+    .subscribe({
+      complete: () => {
+        t.pass()
+      },
+    })
+})
+
+test('combineLatest no completion', (t) => {
+  t.plan(1)
+  const subscription = rxjs
+    .combineLatest([1, 2, 3].map((x) => rxjs.timer(0, 1e3).pipe(rxjs.map(() => x))))
+    .subscribe({
+      next: () => {
+        t.pass()
+      },
+      complete: () => {
+        t.fail()
+      },
+    })
+  t.teardown(() => subscription.unsubscribe())
+})
+
+test('combineMap no completion', (t) => {
+  t.plan(1)
+  const subscription = rxjs
+    .of([1, 2, 3])
+    .pipe(combineMap((x) => rxjs.timer(0, 1e3).pipe(rxjs.map(() => x))))
+    .subscribe({
+      next: () => {
+        t.pass()
+      },
+      complete: () => {
+        t.fail()
+      },
+    })
+  t.teardown(() => subscription.unsubscribe())
+})
+
+test('combineLatest no value', (t) => {
+  t.plan(1)
+  rxjs.combineLatest([1, 2, 3].map((x) => rxjs.EMPTY)).subscribe({
+    complete: () => {
+      t.pass()
+    },
+  })
+})
+
+test('combineMap no value', (t) => {
+  t.plan(1)
+  rxjs
+    .of([1, 2, 3])
+    .pipe(combineMap((x) => rxjs.EMPTY))
+    .subscribe({
+      complete: () => {
+        t.pass()
+      },
+    })
+})
