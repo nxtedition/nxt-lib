ab2cd55fda28a75b3ccda9d7d682ded75ce8894f
Robert Nagy <ronagy@icloud.com>
2022-07-15 14:22:04 +0200

    fix(trace): reduce connections

--- trace.js
+++ trace.js
@@ -13,7 +13,7 @@ module.exports = function ({
   const client = new Pool(Array.isArray(url) ? url[0] : url, {
     keepAliveTimeout: 10 * 60e3,
     pipelining: 8,
-    connections: 4,
+    connections: 1,
   })
 
   destroyers?.push(() => client.close())
