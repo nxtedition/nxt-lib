c216080864c3db9ed08191ed8cb52e000b298ea9
Konstantin M <the.real.laplace@gmail.com>
2022-07-28 11:05:04 +0300

    improve compare-revs (#33)
    * improve compare-revs
    
    * simplify
    
    * simplify2
    
    * simplify
    
    * fixes + test
    
    * wip
    
    * wip
    
    * cleanup
    
    * cleanup
    
    * fixup
    
    Co-authored-by: Robert Nagy <ronagy@icloud.com>

---
+++ test/util/compare-rev.js
@@ -0,0 +1,93 @@
+const { test } = require('tap')
+const { compareRev } = require('../../util')
+
+const cases = [
+  [null, null, 0],
+  [null, '1-00000000000000', -1],
+  ['1-00000000000000', null, 1],
+  ['INF-00000000000000', '1-00000000000000', 1],
+  ['1-00000000000000', 'INF-00000000000000', -1],
+  ['INF-00000000000000', 'INF-00000000000000', 0],
+  ['INF-00000000000001', 'INF-00000000000000', 1],
+  ['INF-00000000000001', 'INF-00000000000002', -1],
+  ['1-00000000000000', '1-00000000000000', 0],
+  ['1-00000000000000', '1-00000000000001', -1],
+  ['1-00000000000001', '1-00000000000000', 1],
+  ['1-00000000000000', '2-00000000000000', -1],
+  ['2-00000000000000', '1-00000000000000', 1],
+  ['1-00000000000000', '02-00000000000000', -1],
+  ['02-00000000000000', '1-00000000000000', 1],
+  ['01-00000000000000', '02-00000000000000', -1],
+  ['02-00000000000000', '01-00000000000000', 1],
+  ['11-00000000000000', '2-00000000000000', 1],
+  ['2-00000000000000', '11-00000000000000', -1],
+]
+
+for (const [a, b, r] of cases) {
+  test(`${a} ${b} ${r}`, (t) => {
+    const expected = _compareRev(a, b)
+    t.same(expected, r) // sanity check
+
+    t.same(Math.sign(compareRev(a, b)), r)
+    t.same(Math.sign(compareRev(a && Buffer.from(a), b)), r)
+    t.same(Math.sign(compareRev(a, b && Buffer.from(b))), r)
+    t.same(Math.sign(compareRev(a && Buffer.from(a), b && Buffer.from(b))), r)
+    t.end()
+  })
+}
+
+const ascii = Array.from({ length: 128 })
+  .map((_, index) => String.fromCharCode(index))
+  .join('')
+const rand = (n) => Math.floor(Math.random() * n)
+const randHex = () => ascii[rand(ascii.length)]
+const randId = () => Array.from({ length: rand(10e2) }, randHex).join('')
+const randRev = () => `${String(rand(10e3)).padStart(rand(10), '0')}-${randId()}`
+
+test('fuzz', (t) => {
+  const N = 1e4
+  for (let i = 0; i < N; i++) {
+    const a = randRev()
+    const b = randRev()
+    const r = _compareRev(a, b)
+
+    t.test(`${a} ${b} ${r}`, (t) => {
+      t.same(Math.sign(compareRev(a, b)), r)
+      t.same(Math.sign(compareRev(a && Buffer.from(a), b)), r)
+      t.same(Math.sign(compareRev(a, b && Buffer.from(b))), r)
+      t.same(Math.sign(compareRev(a && Buffer.from(a), b && Buffer.from(b))), r)
+      t.end()
+    })
+  }
+  t.end()
+})
+
+function _compareRev(a, b) {
+  if (!a) {
+    return b ? -1 : 0
+  }
+
+  if (!b) {
+    return a ? 1 : 0
+  }
+
+  if (a === b) {
+    return 0
+  }
+
+  const av = a[0] === 'I' ? Infinity : parseInt(a)
+  const bv = b[0] === 'I' ? Infinity : parseInt(b)
+
+  if (av !== bv) {
+    return av > bv ? 1 : -1
+  }
+
+  const ar = a.slice(a.indexOf('-') + 1)
+  const br = b.slice(b.indexOf('-') + 1)
+
+  if (ar !== br) {
+    return ar > br ? 1 : -1
+  }
+
+  return 0
+}
--- util/compare-rev.js
+++ util/compare-rev.js
@@ -1,29 +1,221 @@
-module.exports = function compareRev(a, b) {
-  if (!a) {
-    return b ? -1 : 0
+const ordI = 'I'.charCodeAt(0)
+const ordZero = '0'.charCodeAt(0)
+const ordDash = '-'.charCodeAt(0)
+
+const compareRevStringString = (a, b) => {
+  // Handle INF-XXXXXXXX
+  {
+    const isInfA = a[0] === 'I'
+    const isInfB = b[0] === 'I'
+    if (isInfA !== isInfB) {
+      return isInfB ? -1 : 1
+    }
   }
 
-  if (!b) {
-    return a ? 1 : 0
+  let indexA = 0
+  const endA = a.length
+  let lenA = endA
+
+  let indexB = 0
+  const endB = b.length
+  let lenB = endB
+
+  // Skip leading zeroes
+  while (a[indexA] === '0') {
+    ++indexA
+    --lenA
+  }
+  while (b[indexB] === '0') {
+    ++indexB
+    --lenB
   }
 
+  // Compare the revision number
+  let result = 0
+  const end = Math.min(endA, endB)
+  while (indexA < end) {
+    const ac = a[indexA++]
+    const bc = b[indexB++]
+
+    if (ac === '-') {
+      if (bc === '-') {
+        break
+      }
+      return -1
+    } else if (bc === '-') {
+      return 1
+    }
+
+    result ||= ac === bc ? 0 : ac < bc ? -1 : 1
+  }
+
+  if (result) {
+    return result
+  }
+
+  // Compare the rest
+  while (indexA < end) {
+    const ac = a[indexA++]
+    const bc = b[indexB++]
+    if (ac !== bc) {
+      return ac < bc ? -1 : 1
+    }
+  }
+
+  return lenA - lenB
+}
+
+const compareRevBufferBuffer = (a, b) => {
   if (a === b) {
     return 0
   }
 
-  const av = a[0] === 'I' ? Infinity : parseInt(a)
-  const bv = b[0] === 'I' ? Infinity : parseInt(b)
-
-  if (av !== bv) {
-    return av > bv ? 1 : -1
+  // Handle INF-XXXXXXXX
+  {
+    const isInfA = a[0] === ordI
+    const isInfB = b[0] === ordI
+    if (isInfA !== isInfB) {
+      return isInfB ? -1 : 1
+    }
   }
 
-  const ar = a.slice(a.indexOf('-') + 1)
-  const br = b.slice(b.indexOf('-') + 1)
+  let indexA = 0
+  const endA = a.length
+  let lenA = endA
 
-  if (ar !== br) {
-    return ar > br ? 1 : -1
+  let indexB = 0
+  const endB = b.length
+  let lenB = endB
+
+  // Skip leading zeroes
+  while (a[indexA] === ordZero) {
+    ++indexA
+    --lenA
+  }
+  while (b[indexB] === ordZero) {
+    ++indexB
+    --lenB
   }
 
-  return 0
+  // Compare the revision number
+  let result = 0
+  const end = Math.min(endA, endB)
+  while (indexA < end) {
+    const ac = a[indexA++]
+    const bc = b[indexB++]
+
+    const isDashA = ac === ordDash
+    const isDashB = bc === ordDash
+    if (isDashA) {
+      if (isDashB) {
+        break
+      }
+      return -1
+    } else if (isDashB) {
+      return 1
+    }
+
+    result ||= ac - bc
+  }
+
+  if (result) {
+    return result
+  }
+
+  // Compare the rest
+  while (indexA < end) {
+    const ac = a[indexA++]
+    const bc = b[indexB++]
+    result = ac - bc
+    if (result) {
+      return result
+    }
+  }
+
+  return lenA - lenB
+}
+
+const compareRevBufferString = (a, b) => {
+  // Handle INF-XXXXXXXX
+  {
+    const isInfA = a[0] === ordI
+    const isInfB = b[0] === 'I'
+    if (isInfA !== isInfB) {
+      return isInfB ? -1 : 1
+    }
+  }
+
+  let indexA = 0
+  const endA = a.length
+  let lenA = endA
+
+  let indexB = 0
+  const endB = b.length
+  let lenB = endB
+
+  // Skip leading zeroes
+  while (a[indexA] === ordZero) {
+    ++indexA
+    --lenA
+  }
+  while (b[indexB] === '0') {
+    ++indexB
+    --lenB
+  }
+
+  // Compare the revision number
+  let result = 0
+  const end = Math.min(endA, endB)
+  while (indexA < end) {
+    const ac = String.fromCharCode(a[indexA++])
+    const bc = b[indexB++]
+
+    const isDashA = ac === '-'
+    const isDashB = bc === '-'
+    if (isDashA) {
+      if (isDashB) {
+        break
+      }
+      return -1
+    } else if (isDashB) {
+      return 1
+    }
+
+    result ||= ac === bc ? 0 : ac < bc ? -1 : 1
+  }
+
+  if (result) {
+    return result
+  }
+
+  // Compare the rest
+  while (indexA < end) {
+    const ac = String.fromCharCode(a[indexA++])
+    const bc = b[indexB++]
+    if (ac !== bc) {
+      return ac < bc ? -1 : 1
+    }
+  }
+
+  return lenA - lenB
+}
+
+module.exports = function (a, b) {
+  // Handle null and undefined
+  if (!a || !a.length) {
+    return !b || !b.length ? 0 : -1
+  } else if (!b || !b.length) {
+    return 1
+  }
+
+  const isStringA = typeof a === 'string'
+  const isStringB = typeof b === 'string'
+
+  return isStringA
+    ? isStringB
+      ? compareRevStringString(a, b)
+      : -compareRevBufferString(b, a)
+    : isStringB
+    ? compareRevBufferString(a, b)
+    : compareRevBufferBuffer(a, b)
 }
