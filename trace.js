c9e2727d7413453bcd0ee4c9b863bd428fc6a5db
Robert Nagy <ronagy@icloud.com>
2022-08-03 18:24:06 +0200

    perf(trace): reduce num connections

--- trace.js
+++ trace.js
@@ -19,7 +19,7 @@ module.exports = function ({
   const client = new Pool(Array.isArray(url) ? url[0] : url, {
     keepAliveTimeout: 10 * 60e3,
     pipelining: 4,
-    connections: 4,
+    connections: 1,
   })
 
   destroyers?.push(() => client.close())
