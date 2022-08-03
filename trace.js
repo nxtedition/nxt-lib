d1d8a93539197622b76d5e19075c356ecd35eea1
Robert Nagy <ronagy@icloud.com>
2022-08-03 13:22:38 +0200

    fix: trace

--- trace.js
+++ trace.js
@@ -13,51 +13,56 @@ module.exports = function ({
 }) {
   const HEADERS = ['content-type', 'application/x-ndjson']
 
+  let bytes = 0
+  let dropped = 0
+
   const client = new Pool(Array.isArray(url) ? url[0] : url, {
     keepAliveTimeout: 10 * 60e3,
     pipelining: 4,
     connections: 4,
   })
 
-  let dropped = 0
-
   destroyers?.push(() => client.close())
 
   let prefix = ''
-  function updatePrefix() {
+  function clearPrefix() {
     prefix = ''
   }
 
   let traceData = ''
   async function flushTraces() {
-    if (!traceData) {
+    const data = traceData
+    traceData = ''
+
+    if (bytes > LIMIT) {
+      dropped += 1
       return
     }
 
     try {
-      const requestBody = traceData
-      traceData = ''
-
-      const { body } = await client.request({
-        throwOnError: true,
-        path: '/_bulk',
-        method: 'POST',
-        idempotent: true,
-        headers: HEADERS,
-        body: requestBody,
-      })
-      await body.dump()
-
-      if (dropped && traceData.length < LIMIT) {
-        logger.error({ count: dropped }, 'dropped trace messages')
-        dropped = 0
-      }
+      bytes += data.length * 2
+      await client
+        .request({
+          throwOnError: true,
+          path: '/_bulk',
+          method: 'POST',
+          idempotent: true,
+          headers: HEADERS,
+          body: data,
+        })
+        .then(({ body }) => body.dump())
     } catch (err) {
       logger.error({ err }, 'trace failed')
+    } finally {
+      bytes -= data.length * 2
+      if (bytes < LIMIT && dropped) {
+        logger.error({ count: dropped }, 'trace dropped')
+        dropped = 0
+      }
     }
   }
 
-  setInterval(updatePrefix, 1e3).unref()
+  setInterval(clearPrefix, 1e3).unref()
   setInterval(flushTraces, 30e3).unref()
 
   function trace(obj, op) {
@@ -72,16 +77,12 @@ module.exports = function ({
       throw new Error('invalid property `@timestamp`')
     }
 
-    if (traceData.length > LIMIT) {
-      dropped += 1
-      return
-    }
-
     if (!prefix) {
       prefix = `{ "create": { "_index": "${index}" } }\n{ "@timestamp": "${new Date().toISOString()}", "worker": "${serviceName}", "op": "`
     }
 
     const doc = (typeof obj === 'string' ? obj : stringify(obj)).slice(1, -1)
+
     traceData += prefix + `${op}", ${doc} }\n`
     if (traceData.length > BATCH) {
       flushTraces()
