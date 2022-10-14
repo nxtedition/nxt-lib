c92455712d841825b9bb464c42790a9834722ebd
Robert Nagy <ronagy@icloud.com>
2022-10-14 15:20:14 +0200

    refactor(expression): cleanup and optimize

--- util/template/javascript.js
+++ util/template/javascript.js
@@ -17,6 +17,46 @@ const kWait = Symbol('kWait')
 const maxInt = 2147483647
 
 module.exports = ({ ds } = {}) => {
+  function makeTimerEntry(key, refresh, delay) {
+    return {
+      key,
+      timer: setTimeout(refresh, delay),
+      dispose: disposeTimerEntry,
+    }
+  }
+
+  function disposeTimerEntry() {
+    clearTimeout(this.timer)
+    this.timer = null
+  }
+
+  function makeRecordEntry(key, refresh) {
+    const entry = {
+      key,
+      refresh,
+      record: ds.record.getRecord(key),
+      dispose: disposeRecordEntry,
+    }
+    entry.record.on('update', refresh)
+    return entry
+  }
+
+  function disposeRecordEntry() {
+    this.record.unref()
+    this.record.off('update', this.refresh)
+    this.record = null
+  }
+
+  function pipe(value, ...fns) {
+    for (const fn of fns) {
+      value = fn(value)
+      if (value == null) {
+        return undefined
+      }
+    }
+    return value
+  }
+
   return weakCache((expression) => {
     try {
       const script = new vm.Script(`"use strict"; ${expression}`)
@@ -26,11 +66,34 @@ module.exports = ({ ds } = {}) => {
           // TODO (perf): This could be faster by using an array + indices.
           // A bit similar to how react-hooks works.
           const entries = new Map()
+          const context = vm.createContext({
+            ...globals,
+            $: args,
+            nxt: {
+              get: getRecord,
+              asset: hasAssetType,
+              hash: objectHash,
+              timer: getTimer,
+            },
+            _: Object.assign(pipe, {
+              asset: (type) => (id) => hasAssetType(id, type),
+              ds: (postfix, path, state) => (id) => getRecord(`${id}${postfix}`, path, state),
+              timer: (dueTime) => (dueValue) => getTimer(dueTime, dueValue),
+            }),
+          })
 
           let refreshing = false
           let counter = 0
 
-          const refreshNT = () => {
+          refreshNT()
+
+          return () => {
+            for (const entry of entries.values()) {
+              entry.dispose()
+            }
+          }
+
+          function refreshNT() {
             refreshing = false
             counter = (counter + 1) & maxInt
 
@@ -50,14 +113,25 @@ module.exports = ({ ds } = {}) => {
             }
           }
 
-          const refresh = () => {
+          function refresh() {
             if (!refreshing) {
               refreshing = true
               queueMicrotask(refreshNT)
             }
           }
 
-          const getRecord = (key, path, state) => {
+          function getEntry(key, factory, opaque) {
+            let entry = entries.get(key)
+            if (!entry) {
+              entry = factory(key, refresh, opaque)
+              entries.set(key, entry)
+            } else {
+              entry.counter = counter
+            }
+            return entry
+          }
+
+          function getRecord(key, path, state) {
             if (typeof state === 'string') {
               state = ds.CONSTANTS.RECORD_STATE[state.toUpperCase()]
             }
@@ -67,23 +141,7 @@ module.exports = ({ ds } = {}) => {
                 key.startsWith('{') || key.includes('?') ? ds.record.PROVIDER : ds.record.SERVER
             }
 
-            let entry = entries.get(key)
-            if (!entry) {
-              entry = {
-                key,
-                counter,
-                record: ds.record.getRecord(key),
-                dispose() {
-                  this.record.unref()
-                  this.record.off('update', refresh)
-                  this.record = null
-                },
-              }
-              entry.record.on('update', refresh)
-              entries.set(key, entry)
-            } else {
-              entry.counter = counter
-            }
+            const entry = getEntry(key, makeRecordEntry)
 
             if (entry.record.state < state) {
               throw kWait
@@ -92,12 +150,12 @@ module.exports = ({ ds } = {}) => {
             return entry.record.get(path)
           }
 
-          const hasAssetType = (id, type) => {
+          function hasAssetType(id, type) {
             const types = getRecord(id + ':asset.rawTypes?', 'value')
             return types.includes(type) ? id : null
           }
 
-          const getTimer = (dueTime, dueValue = dueTime, undueValue = null) => {
+          function getTimer(dueTime, dueValue = dueTime, undueValue = null) {
             dueTime = Number.isFinite(dueTime) ? dueTime : new Date(dueTime).valueOf()
 
             const nowTime = Date.now()
@@ -110,60 +168,13 @@ module.exports = ({ ds } = {}) => {
               return dueValue
             }
 
-            const key = objectHash({ dueTime, dueValue, undueValue })
-
-            let entry = entries.get(dueTime)
-            if (!entry) {
-              entry = {
-                key,
-                counter,
-                timer: setTimeout(refresh, dueTime - nowTime),
-                dispose() {
-                  clearTimeout(this.timer)
-                  this.timer = null
-                },
-              }
-              entries.set(key, entry)
-            } else {
-              entry.counter = counter
-            }
+            getEntry(
+              objectHash({ dueTime, dueValue, undueValue }),
+              makeTimerEntry,
+              dueTime - nowTime
+            )
 
             return undueValue
-          }
-
-          const context = vm.createContext({
-            ...globals,
-            $: args,
-            nxt: {
-              get: getRecord,
-              asset: hasAssetType,
-              hash: objectHash,
-              timer: getTimer,
-            },
-            _: Object.assign(
-              (value, ...fns) => {
-                for (const fn of fns) {
-                  value = fn(value)
-                  if (value == null) {
-                    return undefined
-                  }
-                }
-                return value
-              },
-              {
-                asset: (type) => (id) => hasAssetType(id, type),
-                ds: (postfix, path, state) => (id) => getRecord(`${id}${postfix}`, path, state),
-                timer: (dueTime) => (dueValue) => getTimer(dueTime, dueValue),
-              }
-            ),
-          })
-
-          refreshNT()
-
-          return () => {
-            for (const entry of entries.values()) {
-              entry.dispose()
-            }
           }
         })
       }
