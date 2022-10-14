9c4bbd5858d92b5de4db32815afb23e54bf8bc77
Robert Nagy <ronagy@icloud.com>
2022-10-14 12:44:44 +0200

    fix(nxtpression): fixes

--- util/template/javascript.js
+++ util/template/javascript.js
@@ -1,8 +1,8 @@
 const NestedError = require('nested-error-stacks')
 const weakCache = require('../../weakCache')
 const rxjs = require('rxjs')
-const hasha = require('hasha')
 const vm = require('node:vm')
+const objectHash = require('object-hash')
 
 const globals = {
   fp: require('lodash/fp'),
@@ -10,12 +10,7 @@ const globals = {
 }
 
 const kWait = Symbol('kWait')
-
-const hashaint = (value, _options = {}) => {
-  const str = JSON.stringify(value)
-  const hash = hasha(str, _options)
-  return parseInt(hash.slice(-13), 16)
-}
+const maxInt = 2147483647
 
 module.exports = ({ ds } = {}) => {
   return weakCache((expression) => {
@@ -26,21 +21,43 @@ module.exports = ({ ds } = {}) => {
         return new rxjs.Observable((o) => {
           const entries = new Map()
 
-          let dirty = false
+          let refreshing = false
+          let counter = 0
 
-          const refresh = () => {
-            if (!dirty) {
-              dirty = true
-              setImmediate(callScript)
+          const refreshNT = () => {
+            refreshing = false
+            counter = (counter + 1) & maxInt
+
+            try {
+              o.next(script.runInContext(context))
+            } catch (err) {
+              if (err !== kWait) {
+                o.error(err)
+              }
+            }
+
+            for (const entry of entries.values()) {
+              if (entry.counter !== counter) {
+                entry.dispose()
+                entries.delete(entry.key)
+              }
             }
           }
 
-          const getRecord = (recordId, path, state = ds.record.SERVER) => {
-            let entry = entries.get(recordId)
+          const refresh = () => {
+            if (!refreshing) {
+              refreshing = true
+              queueMicrotask(refreshNT)
+            }
+          }
+
+          const getRecord = (key, path, state = ds.record.SERVER) => {
+            let entry = entries.get(key)
             if (!entry) {
               entry = {
-                isUsed: false,
-                record: ds.record.getRecord(recordId),
+                key,
+                counter,
+                record: ds.record.getRecord(key),
                 dispose() {
                   this.record.unref()
                   this.record.off('update', refresh)
@@ -48,9 +65,10 @@ module.exports = ({ ds } = {}) => {
                 },
               }
               entry.record.on('update', refresh)
-              entries.set(recordId, entry)
+              entries.set(key, entry)
+            } else {
+              entry.counter = counter
             }
-            entry.isUsed = true
 
             if (typeof state === 'string') {
               state = ds.CONSTANTS.RECORD_STATE[state.toUpperCase()]
@@ -58,9 +76,7 @@ module.exports = ({ ds } = {}) => {
 
             if (state == null) {
               state =
-                recordId.startsWith('{') || recordId.includes('?')
-                  ? ds.record.PROVIDER
-                  : ds.record.SERVER
+                key.startsWith('{') || key.includes('?') ? ds.record.PROVIDER : ds.record.SERVER
             }
 
             if (entry.record.state < state) {
@@ -75,86 +91,68 @@ module.exports = ({ ds } = {}) => {
             return types.includes(type) ? id : null
           }
 
-          const getTimer = (dueTime, undueValue, dueValue) => {
-            dueTime = Number.isFinite(dueTime)
-              ? dueTime
-              : typeof dueTime?.valueOf === 'function'
-              ? dueTime.valueOf()
-              : 0
+          const getTimer = (dueTime, dueValue = dueTime, undueValue = null) => {
+            dueTime = Number.isFinite(dueTime) ? dueTime : new Date(dueTime).valueOf()
+
             const nowTime = Date.now()
+
+            if (!Number.isFinite(dueTime)) {
+              return undueValue
+            }
 
             if (nowTime >= dueTime) {
               return dueValue
             }
 
-            const delay = dueTime - nowTime
+            const key = objectHash({ dueTime, dueValue, undueValue })
 
             let entry = entries.get(dueTime)
             if (!entry) {
               entry = {
-                isUsed: false,
-                timer: setTimeout(refresh, delay),
+                key,
+                counter,
+                timer: setTimeout(refresh, dueTime - nowTime),
                 dispose() {
                   clearTimeout(this.timer)
                   this.timer = null
                 },
               }
-              entries.set(dueTime, entry)
+              entries.set(key, entry)
+            } else {
+              entry.counter = counter
             }
-            entry.isUsed = true
 
             return undueValue
           }
-
-          const _ = (value, ...funcs) => {
-            for (const func of funcs) {
-              value = func(value)
-              if (value == null) {
-                break
-              }
-            }
-            return value
-          }
-
-          _.asset = (type) => (id) => hasAssetType(id, type)
-          _.ds = (domain, path, state) => (id) => getRecord(`${id}:${domain}`, path, state)
 
           const context = vm.createContext({
             ...globals,
             $: args,
             nxt: {
-              ds: getRecord,
+              get: getRecord,
               asset: hasAssetType,
-              hashaint,
+              hash: objectHash,
               timer: getTimer,
-              _,
+              _: Object.assign(
+                (value, ...fns) => {
+                  for (const fn of fns) {
+                    value = fn(value)
+                    if (value == null) {
+                      return undefined
+                    }
+                  }
+                  return value
+                },
+                {
+                  asset: (type) => (id) => hasAssetType(id, type),
+                  ds: (domain, path, state) => (id) => getRecord(`${id}:${domain}`, path, state),
+                  timer: (dueTime) => (dueValue) => getTimer(dueTime, dueValue),
+                }
+              ),
             },
           })
 
-          const callScript = () => {
-            dirty = false
-
-            for (const entry of entries.values()) {
-              entry.isUsed = false
-            }
-
-            try {
-              o.next(script.runInContext(context))
-            } catch (err) {
-              if (err !== kWait) {
-                o.error(err)
-              }
-            }
-
-            for (const [key, entry] of entries.entries()) {
-              if (!entry.isUsed) {
-                entry.dispose()
-                entries.delete(key)
-              }
-            }
-          }
-
-          callScript()
+          refreshNT()
 
           return () => {
             for (const entry of entries.values()) {
