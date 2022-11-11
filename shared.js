0ff88ff17c2478462cd4aeed06a68952e258ac09
Robert Nagy <ronagy@icloud.com>
2022-11-11 15:31:42 +0100

    fix: allow 0 len

--- shared.js
+++ shared.js
@@ -146,7 +146,7 @@ function writer({ sharedState, sharedBuffer, logger }) {
     const dataPos = writePos + 4
     const dataLen = fn(dataPos, buffer, arg1, arg2, arg3) - dataPos
 
-    assert(dataLen > 0 && dataLen <= len + 4)
+    assert(dataLen >= 0 && dataLen <= len + 4)
     assert(dataPos + dataLen <= size)
     assert((dataPos & 0x3) === 0)
 
