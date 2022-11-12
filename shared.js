2bbbedc2f70b8c29624402d495ed8c14bbccc9f1
Robert Nagy <ronagy@icloud.com>
2022-11-12 20:44:35 +0100

    Revert "feat(shared): pollInterval"
    This reverts commit 83441b5037a886ecd3545df0fa7e582160842c60.

--- shared.js
+++ shared.js
@@ -25,7 +25,7 @@ let poolSize = 1024 * 1024
 let poolOffset = 0
 let poolBuffer = Buffer.allocUnsafeSlow(poolSize).buffer
 
-async function reader({ sharedState, sharedBuffer, pollInterval }, cb) {
+async function reader({ sharedState, sharedBuffer }, cb) {
   const state = new Int32Array(sharedState)
   const buffer32 = new Int32Array(sharedBuffer)
   const size = sharedBuffer.byteLength
@@ -72,12 +72,7 @@ async function reader({ sharedState, sharedBuffer, pollInterval }, cb) {
 
     Atomics.store(state, READ_INDEX, readPos)
 
-    if (pollInterval != null) {
-      await tp.setTimeout(pollInterval)
-    } else {
-      await tp.setImmediate()
-    }
-
+    await tp.setImmediate()
     writePos = Atomics.load(state, WRITE_INDEX)
   }
 }
