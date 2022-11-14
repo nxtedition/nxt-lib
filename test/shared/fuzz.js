da58c00fc3b9bb83670bfcf3ee827b49d3eb91b4
Robert Nagy <ronagy@icloud.com>
2022-11-14 09:53:03 +0100

    tests

--- test/shared/fuzz.js
+++ test/shared/fuzz.js
@@ -20,33 +20,40 @@ function fuzz(seed) {
 
   const cnt = Math.floor(Math.random() * 10000)
   for (let n = 0; n < cnt; n++) {
-    if (random() > 0.5) {
-      const writeSize = randInt(16, size - 64)
-      write(writeSize, (dataPos, buffer) => {
-        const value = writeIdx++
-        values.push(value)
-        buffer.fill(0, dataPos, dataPos + writeSize)
-        buffer.writeInt32LE(value, dataPos)
-        return dataPos + writeSize
-      })
-    } else {
-      read((buffer, dataPos, dataLen) => {
-        const value = buffer.readInt32LE(dataPos)
-        const expected = values[readIdx++]
-        assert.equal(value, expected, `readIdx: ${readIdx}, value: ${value}, expected: ${expected}`)
-      })
+    let value
+    let expected
+    try {
+      if (random() > 0.5) {
+        const writeSize = randInt(16, size - 64)
+        write(writeSize, (dataPos, buffer) => {
+          const value = writeIdx++
+          values.push(value)
+          buffer.fill(0, dataPos, dataPos + writeSize)
+          buffer.writeInt32LE(value, dataPos)
+          return dataPos + writeSize
+        })
+      } else {
+        read((buffer, dataPos, dataLen) => {
+          value = buffer.readInt32LE(dataPos)
+          expected = values[readIdx++]
+          assert.equal(value, expected)
+        })
+      }
+    } catch (err) {
+      console.error(err, `readIdx: ${readIdx}, value: ${value}, expected: ${expected}, ${n}`)
+      return false
     }
   }
+
+  return true
 }
 
 let seed = 0
-try {
-  const startTime = Date.now()
-  while (Date.now() - startTime < 30e3) {
-    fuzz(seed++)
+const startTime = Date.now()
+while (Date.now() - startTime < 30e3) {
+  if (!fuzz(seed++)) {
+    break
   }
-} catch (err) {
-  console.error(err)
 }
 
 console.log('seed', seed)
