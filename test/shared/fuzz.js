cf2ac08d3973ab7ef369eef7a310bc033362b2a2
Konstantin M <the.real.laplace@gmail.com>
2022-11-15 10:05:51 +0200

    tests for shared.js + bugfix (#40)

--- test/shared/fuzz.js
+++ test/shared/fuzz.js
@@ -1,72 +1,89 @@
+const { test } = require('tap')
 const { alloc, reader, writer } = require('../../shared.js')
-const assert = require('node:assert')
+const { setTimeout } = require('node:timers/promises')
 
-function fuzz(seed) {
-  function random() {
-    return Math.abs(Math.sin(seed++))
-  }
+const RANGE = 1 - Number.EPSILON
+const makeRandom = (seed) => {
+  const random = () => ((Math.sin(seed++) + 1) / 2) * RANGE
   const rand = (n) => Math.floor(random() * n)
   const randInt = (a, b) => rand(b - a + 1) + a
+  return { random, rand, randInt }
+}
 
-  const size = randInt(64, 256) * 4
-  const shared = alloc(size)
-  const write = writer(shared)
-  const read = reader(shared)
+const fuzz = (seed) =>
+  new Promise((resolve) => {
+    test(`seed ${seed}`, async (t) => {
+      const { random, rand, randInt } = makeRandom(seed)
 
-  let writeIdx = 0
-  let readIdx = 0
-  const values = []
+      const size = randInt(64, 256) * 4
+      const shared = alloc(size)
+      const write = writer(shared)
+      const read = reader(shared)
 
-  const cnt = randInt(1000, 10000)
-  for (let n = 0; n < cnt; n++) {
-    let value
-    let expected
-    let writeSize
-    try {
-      if (random() > 0.5) {
-        writeSize = randInt(16, size - 64)
-        write(writeSize, (dataPos, buffer) => {
-          const value = writeIdx++
-          values.push(value)
-          buffer.fill(0, dataPos, dataPos + writeSize)
-          buffer.writeInt32LE(value, dataPos)
-          return dataPos + writeSize
-        })
-      } else {
-        read((buffer, dataPos, dataLen) => {
-          value = buffer.readInt32LE(dataPos)
-          expected = values[readIdx++]
-          assert.equal(value, expected)
-        })
-      }
-    } catch (err) {
-      console.error(
-        err,
-        JSON.stringify(
-          {
-            value,
-            expected,
-            writeSize,
-            writeIdx,
-            readIdx,
-            n,
-          },
-          undefined,
-          2
+      let writeIdx = 0
+      let readIdx = 0
+      const values = []
+
+      let value
+      let expected
+      let writeSize
+      try {
+        const cnt = randInt(1000, 10000)
+        for (let n = 0; n < cnt; n++) {
+          if (random() > 0.5) {
+            writeSize = randInt(16, Math.floor(size / 4))
+            write(writeSize, (dataPos, buffer) => {
+              const value = rand(0x7fffffff)
+              values[writeIdx++] = value
+              buffer.fill(0, dataPos, dataPos + writeSize)
+              buffer.writeInt32LE(value, dataPos)
+              return dataPos + writeSize
+            })
+          } else {
+            read((buffer, dataPos, dataLen) => {
+              value = buffer.readInt32LE(dataPos)
+              expected = values[readIdx++]
+              t.equal(value, expected)
+            })
+          }
+        }
+
+        while (readIdx !== writeIdx) {
+          read((buffer, dataPos, dataLen) => {
+            value = buffer.readInt32LE(dataPos)
+            expected = values[readIdx++]
+            t.equal(value, expected)
+          })
+
+          await setTimeout(200)
+        }
+      } catch (err) {
+        console.error(
+          err,
+          JSON.stringify(
+            {
+              value,
+              expected,
+              writeSize,
+              writeIdx,
+              readIdx,
+            },
+            undefined,
+            2
+          )
         )
-      )
-      return false
-    }
+        t.fail()
+      }
+      t.end()
+      resolve()
+    })
+  })
+
+const { rand } = makeRandom(0)
+const makeSeed = () => rand(0xffff)
+
+;(async () => {
+  for (let i = 0; i < 100; i++) {
+    await fuzz(makeSeed())
   }
-
-  return true
-}
-
-let seed = 0
-while (seed < 1000) {
-  if (!fuzz(seed++)) {
-    break
-  }
-}
-
-console.log('seed', seed)
+})()
