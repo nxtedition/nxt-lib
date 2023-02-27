3b0325f9638eb01d873dd1ad8c63f0a4ac31118e
Robert Nagy <ronagy@icloud.com>
2023-02-27 13:50:54 +0100

    feat: elastic

---
+++ elasticsearch.js
@@ -0,0 +1,28 @@
+import Elasticsearch from '@elastic/elasticsearch'
+import { UndiciConnection } from '@elastic/transport'
+
+// https://github.com/elastic/elasticsearch-js/issues/1716#issuecomment-1167173492
+class Connection extends UndiciConnection {
+  request(params, options) {
+    if (!options.signal) {
+      const ac = new AbortController()
+      options = {
+        ...options,
+        signal: ac.signal,
+      }
+    }
+    return super.request(params, options)
+  }
+}
+
+// Use ClusterConnectionPool until https://github.com/elastic/elasticsearch-js/issues/1714 is addressed.
+// See, https://github.com/elastic/kibana/pull/134628
+const ConnectionPool = Elasticsearch.ClusterConnectionPool
+
+export function makeElasticsearchClient(config) {
+  return new Elasticsearch.Client({
+    Connection,
+    ConnectionPool,
+    ...(config.elasticsearch ?? config),
+  })
+}
