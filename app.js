2fa374867cd48f8bfa7cb2b2a35103d7721fbc0e
Robert Nagy <ronagy@icloud.com>
2023-06-30 12:12:17 +0200

    fix(app): more auto stats

--- app.js
+++ app.js
@@ -550,12 +550,15 @@ module.exports = function (appConfig, onTerminate) {
     }
 
     stats$ = stats$.pipe(
-      rx.map((x) => ({
-        ...x,
-        timestamp: Date.now(),
-      })),
       rx.auditTime(1e3),
       rx.filter(Boolean),
+      rx.map((x) => ({
+        ...x,
+        ds: ds?.stats,
+        couch: couch?.stats,
+        lag: toobusy?.lag(),
+        timestamp: Date.now(),
+      })),
       rx.retryWhen((err$) =>
         err$.pipe(
           rx.tap((err) => logger.error({ err }, 'monitor.stats')),
@@ -575,9 +578,6 @@ module.exports = function (appConfig, onTerminate) {
         const elu2 = eventLoopUtilization?.()
         logger.debug(
           {
-            ds: ds?.stats,
-            couch: couch?.stats,
-            lag: toobusy?.lag(),
             memory: process.memoryUsage(),
             utilization: eventLoopUtilization?.(elu2, elu1),
             heap: v8.getHeapStatistics(),
